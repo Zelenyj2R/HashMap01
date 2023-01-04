a2d3ba749bd7acf75d8dd0f279aad2ec81b6df4e
Artem Knyzhnykov <111915140+Zelenyj2R@users.noreply.github.com>
2023-01-04 19:09:51 +0100

    Update HashMapTest.java

--- src/HashMapTest.java
+++ src/HashMapTest.java
@@ -1,21 +1,13 @@
-
-
 public class HashMapTest {
     public static void main(String[] args) {
-
-
         MyHashMap<Integer, String> myHashMap = new MyHashMap<>();
-
         System.out.println("myHashMap  " + myHashMap.size());
         myHashMap.put(1, "First");
         myHashMap.put(2, "Second");
         myHashMap.put(3, "Third");
         myHashMap.put(4, "Fourth");
-
         System.out.println("myHashMap  " + myHashMap.size());
         System.out.println("myHashMap  " + myHashMap.get(3));
-
-
         myHashMap.remove(3);
         System.out.println("myHashMap  " + myHashMap.size());
         myHashMap.put(3, "Third");
